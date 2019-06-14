5aec6d4b326b328fa0717f06be39ae1a3fe72d14
Izaflps <izaflps@gmail.com>
2019-06-13 21:48:14 -0300

    Commit 2

---
+++ OitoPontoDois/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OitoPontoDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp, resp2;
+            Console.WriteLine("Deseja comprar algum item?");
+            resp = Console.ReadLine();
+            if (resp == "Não")
+            {
+                Console.WriteLine("Operação finalizada.");
+
+            }
+            else
+            {
+                do
+                {
+                    Console.WriteLine("Deseja comprar mais alguma coisa?");
+                    resp2 = Console.ReadLine();
+
+                } while (resp2 == "Sim");
+            }
+        }
+    }
+}
